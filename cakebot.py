2a017dee507344033c9fc07e99b08c0dd7df2d73
andrei G <sinezub@yandex.ru>
2024-06-13 10:56:15 +0300

    Updated tg cake bot.

--- cakebot.py
+++ cakebot.py
@@ -1,172 +1,156 @@
+import os
 import logging
 
 import cohere
 
+from dotenv import load_dotenv
 from telegram import ForceReply, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
-    Application, 
-    CommandHandler, 
-    ContextTypes, 
+    Application,
+    CommandHandler,
+    ContextTypes,
     ConversationHandler,
-    MessageHandler, 
+    MessageHandler,
     filters)
 
+from orders import actual_orders
+
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 logging.getLogger('httpx').setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
-COHERE_TOKEN = ''
-TG_TOKEN = ''
+load_dotenv()
 
-GENDER, PHOTO, LOCATION, BIO = range(4)
+COHERE_TOKEN = os.getenv('COHERE_TOKEN')
+TG_TOKEN = os.getenv('TG_TOKEN')
+
+BISCUIT, TOPPINGS, PHOTO, EXTRA = range(4)
 
 llm_client = cohere.Client(COHERE_TOKEN)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Starts the conversation and asks the user about their gender."""
-    reply_keyboard = [["Boy", "Girl", "Other"]]
 
-    await update.message.reply_text(
-        "Hi! My name is Professor Bot. I will hold a conversation with you. "
-        "Send /cancel to stop talking to me.\n\n"
-        "Are you a boy or a girl?",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
-        ),
-    )
+class Bot:
+    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        """Starts the conversation and asks the user about biscuit."""
+        reply_keyboard = [['Ванильный', 'Радуга', 'Шоколадный']]
 
-    return GENDER
+        await update.message.reply_text(
+            'Привет! Я конструктор тортов. Помогу вам собрать свой тортик.'
+            'Отправь /cancel чтобы остановить диалог.\n\n'
+            'Какой вам нужен бисквит?',
+            reply_markup=ReplyKeyboardMarkup(
+                reply_keyboard, 
+                one_time_keyboard=True, 
+                input_field_placeholder='Выберите бисквит'
+            ),
+        )
+        return BISCUIT
 
+    async def biscuit(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        return TOPPINGS
 
-async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the selected gender and asks for a photo."""
-    user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text(
-        "I see! Please send me a photo of yourself, "
-        "so I know what you look like, or send /skip if you don't want to.",
-        reply_markup=ReplyKeyboardRemove(),
-    )
+    async def toppings(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        """Stores the selected biscuit and asks for toppings."""
+        user = update.message.from_user
+        logger.info("Toppings of %s: %s", user.first_name, update.message.text)
+        await update.message.reply_text(
+            "I see! Please send me a photo of yourself, "
+            "so I know what you look like, or send /skip if you don't want to.",
+            reply_markup=ReplyKeyboardRemove(),
+        )
 
-    return PHOTO
+        return PHOTO
 
+    async def photo(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        """Stores the toppings and asks for a decor examples."""
+        user = update.message.from_user
+        photo_file = await update.message.photo[-1].get_file()
+        await photo_file.download_to_drive("cake_photo.jpg")
+        logger.info("Cake Decor of %s: %s", user.first_name, "cake_photo.jpg")
+        await update.message.reply_text(
+            "Gorgeous! Now, send me your location please, or send /skip if you don't want to."
+        )
 
-async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the photo and asks for a location."""
-    user = update.message.from_user
-    photo_file = await update.message.photo[-1].get_file()
-    await photo_file.download_to_drive("user_photo.jpg")
-    logger.info("Photo of %s: %s", user.first_name, "user_photo.jpg")
-    await update.message.reply_text(
-        "Gorgeous! Now, send me your location please, or send /skip if you don't want to."
-    )
+        return EXTRA
 
-    return LOCATION
+    async def skip_photo(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        """Skips the photo and asks for a location."""
+        user = update.message.from_user
+        logger.info("User %s did not send a photo.", user.first_name)
+        await update.message.reply_text(
+            "I bet you look great! Now, send me your location please, or send /skip."
+        )
 
-
-async def skip_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Skips the photo and asks for a location."""
-    user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    await update.message.reply_text(
-        "I bet you look great! Now, send me your location please, or send /skip."
-    )
-
-    return LOCATION
-
-
-async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the location and asks for some info about the user."""
-    user = update.message.from_user
-    user_location = update.message.location
-    logger.info(
-        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
-    )
-    await update.message.reply_text(
-        "Maybe I can visit you sometime! At last, tell me something about yourself."
-    )
-
-    return BIO
+        return EXTRA
 
 
-async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Skips the location and asks for info about the user."""
-    user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
-    await update.message.reply_text(
-        "You seem a bit paranoid! At last, tell me something about yourself."
-    )
+    async def extra(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        """Stores the info about the user and ends the conversation."""
+        user = update.message.from_user
+        logger.info("Extra of %s: %s", user.first_name, update.message.text)
+        await update.message.reply_text("Thank you! I hope we can talk again some day.")
 
-    return BIO
+        return ConversationHandler.END
+
+    async def cancel(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        """Cancels and ends the conversation."""
+        user = update.message.from_user
+        logger.info("User %s canceled the conversation.", user.first_name)
+        await update.message.reply_text(
+            "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+        )
+
+        return ConversationHandler.END
+
+    async def orders(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        """Gets orders from external gsheet file."""
+        user = update.effective_user
+        orders = actual_orders()
+        message = rf"""
+            Hi {user.mention_markdown_v2()}\!
+            List of cake orders:
+            ```{orders.to_markdown(index=False)}```
+        """
+        await update.message.reply_text(
+            message, parse_mode='MarkdownV2',
+            # reply_markup=ForceReply(selective=True),
+        )
+
+    async def llm(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        # TODO: call gpt/gemini api
+        user_message = update.message.text
+        response = llm_client.chat(message=user_message)
+
+        await update.message.reply_text(f'Hey! I have got response from LLM {response.text}')
 
 
-async def bio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the info about the user and ends the conversation."""
-    user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text("Thank you! I hope we can talk again some day.")
-
-    return ConversationHandler.END
-
-
-async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Cancels and ends the conversation."""
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    await update.message.reply_text(
-        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
-    )
-
-    return ConversationHandler.END
-
-async def start(update, context):
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
-    
-    
-async def echo(update, context):
-    await update.message.reply_text(update.message.text)
-  
-  
-async def llm(update, context):
-    # TODO: call gpt/gemini api
-    user_message = update.message.text
-    response = llm_client.chat(message=user_message)
-    
-    await update.message.reply_text(f'Hey! I have got response from LLM {response.text}')
-    
 def main():
     """Start the bot."""
+    bot = Bot()
     application = Application.builder().token(TG_TOKEN).build()
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('llm', llm))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-    
-        # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    application.add_handler(CommandHandler('orders', bot.orders))
+    application.add_handler(CommandHandler('llm', bot.llm))
+    # application.add_handler(MessageHandler(
+    #     filters.TEXT & ~filters.COMMAND, bot.echo))
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
+        entry_points=[CommandHandler('start', bot.start)],
         states={
-            GENDER: [MessageHandler(filters.Regex("^(Boy|Girl|Other)$"), gender)],
-            PHOTO: [MessageHandler(filters.PHOTO, photo), CommandHandler("skip", skip_photo)],
-            LOCATION: [
-                MessageHandler(filters.LOCATION, location),
-                CommandHandler("skip", skip_location),
-            ],
-            BIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, bio)],
+            BISCUIT: [MessageHandler(filters.Regex('^(Ванильный|Радуга|Шоколадный)$'), bot.toppings)],
+            PHOTO: [MessageHandler(filters.PHOTO, bot.photo), CommandHandler('skip', bot.skip_photo)],
+            EXTRA: [MessageHandler(filters.TEXT & ~filters.COMMAND, bot.extra)],
         },
-        fallbacks=[CommandHandler("cancel", cancel)],
+        fallbacks=[CommandHandler('cancel', bot.cancel)],
     )
 
     application.add_handler(conv_handler)
-    
+
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-    
-    
+
+
 if __name__ == '__main__':
     main()
